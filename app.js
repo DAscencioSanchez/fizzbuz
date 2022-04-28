04c7db43e4afc75eb3df65414dc09149fde26c22
DAscencioSanchez <diaspm77@gmail.com>
2022-04-27 22:41:43 -0500

    Step 6: Refactorizing part5 FizzBuzzService

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Reader = require('./lib/utils/Reader')
 const ExplorerService = require('./lib/services/ExplorerService')
+const FizzBuzzService = require('./lib/services/FizzBuzzService')
 
 // Part 1 Read json file ===========================
 const explorers = Reader.readJsonFile("explorers.json"); // esto refrea la lista de explorers
@@ -8,12 +9,36 @@ const explorers = Reader.readJsonFile("explorers.json"); // esto refrea la lista
 
 // Part 2: Get the explorers in a mission
 const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-console.log(explorersInNode)
+//console.log(explorersInNode)
 
 //Part 3: Get number of explorers in a mission
 const amountExplorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-console.log(amountExplorersByMission)
+//console.log(amountExplorersByMission)
 
 //Part 4: Get number of explorers in a mission
 const ExplorersUsernamesByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
-console.log(ExplorersUsernamesByMission)
+//console.log(ExplorersUsernamesByMission)
+
+// Part 5: Get a list of the explorers in node, if the score is divisible by 5 and 3, 
+//set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik 
+//and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, 
+// otherwise set the property trick and the score value.
+
+const explorer1 = {name: 'Explorer1', score:1}
+const validationExplorer1 = FizzBuzzService.applyValidationInExplorer(explorer1)
+console.log(validationExplorer1) // { name: 'Explorer1', score: 1, trick: 1 }
+
+const explorer3 = {name: 'Explorer3', score:3}
+const validationExplorer3 = FizzBuzzService.applyValidationInExplorer(explorer3)
+console.log(validationExplorer3) // { name: 'Explorer3', score: 3, trick: 'FIZZ' }
+
+const explorer5 = {name: 'Explorer5', score:5}
+const validationExplorer5 = FizzBuzzService.applyValidationInExplorer(explorer5)
+console.log(validationExplorer5) // { name: 'Explorer5', score: 5, trick: 'BUZZ' }
+
+const explorer15 = {name: 'Explorer15', score:15}
+const validationExplorer15 = FizzBuzzService.applyValidationInExplorer(explorer15)
+console.log(validationExplorer15) // { name: 'Explorer15', score: 15, trick: 'FIZZBUZZ' }
+
+
+
