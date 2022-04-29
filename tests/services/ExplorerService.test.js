6927e1002ccc307db7bffebea734b2795a9de7af
DAscencioSanchez <diaspm77@gmail.com>
2022-04-28 21:04:58 -0500

    Fixed issues with npm run linter-fix

--- tests/services/ExplorerService.test.js
+++ tests/services/ExplorerService.test.js
@@ -1,5 +1,4 @@
-const Reader = require('./../../lib/utils/Reader')
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Tests for ExplorerService.js", () => {
 
@@ -9,22 +8,22 @@ describe("Tests for ExplorerService.js", () => {
             "githubUsername": "ajolonauta1",
             "score": 1,
             "mission": "node",
-            },
+        },
         {
             "name": "Woopa2",
             "githubUsername": "ajolonauta2",
             "score": 2,
             "mission": "java",
             
-        }]
+        }];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode).toStrictEqual([{
             "name": "Woopa1",
             "githubUsername": "ajolonauta1",
             "score": 1,
             "mission": "node",
-            }])
+        }]);
     
-    })
+    });
 
-})
+});
--- tests/utils/Reader.test.js
+++ tests/utils/Reader.test.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Tests for Reader.js", () => {
 
     test("1. A test for JSON file Reader", () => {
-        const explorers = Reader.readJsonFile("explorers.json")
+        const explorers = Reader.readJsonFile("explorers.json");
         const rawdata = fs.readFileSync("explorers.json");
         const manualReadExplorers = JSON.parse(rawdata);
-        expect(explorers).toStrictEqual(manualReadExplorers)
-    })
+        expect(explorers).toStrictEqual(manualReadExplorers);
+    });
 
-})
+});
